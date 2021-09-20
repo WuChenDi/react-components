5dec0253694e3602b6a13a6382a08606a64286df
WuChenDi <996194720@qq.com>
2021-09-20 21:42:22 +0800

    feat: adding an upload and tabs component

---
+++ src/components/Tabs/index.tsx
@@ -0,0 +1,13 @@
+import React, { FC } from 'react'
+import Tabs, { TabProps } from './tabs'
+import TabsItem, { TabsItemProps } from './tabsItem'
+
+type TabsComponent = FC<TabProps> & {
+  Item?: FC<TabsItemProps>
+}
+
+let TransTab: TabsComponent = Tabs
+TransTab.Item = TabsItem
+
+// export type {TabProps, TabsItemProps}
+export default TransTab
