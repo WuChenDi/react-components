77b738517d03aaa872ecf0c5246a937a5fed41b3
WuChenDi <wuchendi96@gmail.com>
2023-03-21 17:21:23 +0800

    feat(test): Comment next.config.js code.

--- next.config.js
+++ next.config.js
@@ -3,9 +3,9 @@ import * as path from 'path';
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  sassOptions: {
-    includePaths: [path.join(__dirname, 'styles')],
-  },
+  // sassOptions: {
+  //   includePaths: [path.join(__dirname, 'styles')],
+  // },
 }
 
 module.exports = nextConfig
